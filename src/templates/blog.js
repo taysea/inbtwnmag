7d0f26f73f94e80c8aa2cc2bfd41bf6d17e1ecbf
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-04 20:25:28 -0800

    Adjust blog page for mobile

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,67 +2,72 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Box, Image, Text } from "grommet"
+import { Box, Image, Text, ResponsiveContext } from "grommet"
 import { PartialWidthSection } from "../layouts"
-import { CardFooter } from "../components"
+import { BodyText, CardFooter } from "../components"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types"
-import { BodyText } from "../components/Styled"
 
-// const Bold = ({ children }) => (
-//   <Text weight="bold" color="#222">
-//     {children}
-//   </Text>
-// )
-
-function BlogTemplate({ data }) {
-  return (
-    <Layout>
-      <SEO
-        title={data.contentfulBlog.seo.title}
-        description={data.contentfulBlog.seo.description}
-      />
-      <PartialWidthSection pad={{ top: "medium" }} marginBottom="large">
-        <Box width="large" gap="small" margin="auto" pad={{ top: "medium" }}>
-          <Box gap="medium" align="center">
-            <Text weight="bold" size="2.5em" textAlign="center">
-              {data.contentfulBlog.title}
-            </Text>
-            <CardFooter
-              author={data.contentfulBlog.author}
-              tags={data.contentfulBlog.tags}
-            />
+const BlogTemplate = ({ data }) => (
+  <Layout>
+    <SEO
+      title={data.contentfulBlog.seo.title}
+      description={data.contentfulBlog.seo.description}
+    />
+    <ResponsiveContext.Consumer>
+      {size => (
+        <>
+          <PartialWidthSection pad={{ top: "medium" }} marginBottom="large">
+            <Box
+              width="large"
+              gap="small"
+              margin="auto"
+              pad={{ top: "medium" }}
+            >
+              <Box gap="medium" align="center">
+                <Text weight="bold" size="2.5em" textAlign="center">
+                  {data.contentfulBlog.title}
+                </Text>
+                <CardFooter
+                  author={data.contentfulBlog.author}
+                  tags={data.contentfulBlog.tags}
+                />
+              </Box>
+            </Box>
+          </PartialWidthSection>
+          <Box
+            height={size !== "small" ? "80vh" : "medium"}
+            margin={{ bottom: "medium" }}
+          >
+            <Image src={data.contentfulBlog.titleImage.file.url} fit="cover" />
           </Box>
-        </Box>
-      </PartialWidthSection>
-      <Box height="80vh" margin={{ bottom: "medium" }}>
-        <Image src={data.contentfulBlog.titleImage.file.url} fit="cover" />
-      </Box>
-      <PartialWidthSection>
-        <Box width="large" gap="small" margin="auto">
-          <BodyText>
-            {documentToReactComponents(data.contentfulBlog.body.json, {
-              // renderMark: {
-              //   [MARKS.BOLD]: text => <Bold>{text}</Bold>,
-              // },
-              renderNode: {
-                [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
-                  <Box>
-                    <Image
-                      src={node.data.target.fields.file["en-US"].url}
-                      fit="cover"
-                    />
-                  </Box>
-                ),
-              },
-            })}
-          </BodyText>
-        </Box>
-      </PartialWidthSection>
-      <Box height="30em" background="light-3" />
-    </Layout>
-  )
-}
+          <PartialWidthSection>
+            <Box width="large" gap="small" margin="auto">
+              <BodyText>
+                {documentToReactComponents(data.contentfulBlog.body.json, {
+                  // renderMark: {
+                  //   [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+                  // },
+                  renderNode: {
+                    [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
+                      <Box height={size === "small" ? "medium" : undefined}>
+                        <Image
+                          src={node.data.target.fields.file["en-US"].url}
+                          fit="cover"
+                        />
+                      </Box>
+                    ),
+                  },
+                })}
+              </BodyText>
+            </Box>
+          </PartialWidthSection>
+          <Box height="30em" background="light-3" />
+        </>
+      )}
+    </ResponsiveContext.Consumer>
+  </Layout>
+)
 
 export const query = graphql`
   query blogQuery($slug: String!) {
