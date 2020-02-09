63d720a99f427471cce43ef4ab8b6a392fc9ebf3
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-09 10:34:21 -0800

    Create CategoryLink

---
+++ src/components/Links/CategoryLink.js
@@ -0,0 +1,12 @@
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+export const CategoryLink = styled(Link)`
+  font-weight: 600;
+  text-decoration: none;
+  color: #094533;
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: #094533;
+  }
+`
