26e9f1a210435495e6da5cd8cd0fd5e70a85142e
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-13 08:48:01 -0800

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,12 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-158393091-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
